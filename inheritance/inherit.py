67db2b47148e61fcaa8ba608d9f52d995d3dc375
maandeep98 <maandeep998@gmail.com>
2018-03-02 11:31:08 +0530

    it shows the inheritance of classes

--- inheritance/inherit.py
+++ inheritance/inherit.py
@@ -16,6 +16,11 @@ class Child(Parent):
         Parent.__init__(self, lname, eye_color)
         self.no_of_toys=no_of_toys
 
+    def show_info(self):
+        print("Last Name - "+self.lname)
+        print("Eye Colour - "+self.eye_color)
+        print("No. of toys - "+str(self.no_of_toys))
+        
 miley = Child("Cyrus","blue",5)
 miley.show_info()
 """print(miley.lname)
